c57232e647bf72f562dcbf92c7f5fc060ebc1ad1
Rohit <101079442+Singh8700@users.noreply.github.com>
2025-03-27 13:31:03 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    compiler: {
-      styledComponents: true, // ✅ Ensure styled-components support is enabled
-    },
-    experimental: {
-      appDir: true, // ✅ Ensure App Router support
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
